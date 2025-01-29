3242bf017e6f3f95bad50988c56604aa1d6ee71e
Nicolas M <mbark.nicolas@gmail.com>
2025-01-29 17:46:15 +0100

    add ecosystem config

---
+++ ecosystem.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  apps: [
+    {
+      name: "sos-ses",
+      script: "npm start",
+      cwd: "/home/sos-ses", // On travaille directement dans /home/sos-ses
+      env_production: {
+        PORT: 3000,
+        NODE_ENV: "production",
+      },
+      autorestart: true,
+      max_memory_restart: "1G",
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      merge_logs: true,
+      log_file: "./logs/combined.log",
+    },
+  ],
+  deploy: {
+    production: {
+      user: "root",
+      host: "163.172.160.241",
+      ref: "origin/master",
+      repo: "git@github.com:Tollaris95/sos-ses.git",
+      path: "/home/sos-ses", // Pas de sous-dossier source
+      "pre-deploy-local": "",
+      "post-deploy": "npm install && npm run build && pm2 startOrReload ecosystem.config.js",
+      "pre-setup": "",
+    },
+  },
+};
