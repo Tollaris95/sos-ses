e8b4f05014cb73577284f82e923bcc5a648a56f8
Nicolas M <mbark.nicolas@gmail.com>
2025-01-29 19:14:44 +0100

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: "sos-ses",
-      script: "npm start",
+      script: "npm",
+      args: "start",
       cwd: "/home/sos-ses/source",
       interpreter: "/usr/bin/node",
       env_production: {
@@ -23,18 +24,11 @@ module.exports = {
     production: {
       user: "root",
       host: "163.172.160.241",
-      // Mets "origin/main" si ta branche est main :
       ref: "origin/master",
       repo: "git@github.com:Tollaris95/sos-ses.git",
       path: "/home/sos-ses",
-
-      // Hook local : s'exécute avant l'envoi des fichiers (depuis ta machine)
       "pre-deploy-local": "",
-
-      // Hook distant (sur le serveur) après que le code soit copié
       "post-deploy": "npm install && npm run build && pm2 startOrReload ecosystem.config.js",
-
-      // Éventuellement un hook avant le setup (facultatif)
       "pre-setup": "",
     },
   },
